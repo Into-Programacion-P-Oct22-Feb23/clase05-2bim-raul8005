03e349899421e628773e6e622c9a4c1836772cec
raul8005 <rimedina@utpl.edu.ec>
2023-01-11 12:15:39 -0500

    Clase 05

--- EjemplosRecursividad/src/paquete01/Ejemplo07.java
+++ EjemplosRecursividad/src/paquete01/Ejemplo07.java
@@ -8,7 +8,7 @@ public class Ejemplo07 {
 
     public static void main(String[] args) {
         
-        System.out.printf("%d\n", obtenerPotencia(3, 3));
+        System.out.printf("%d\n", obtenerPotencia(9, 1));
     }
 
     public static int obtenerPotencia(int base, int exponente) {
